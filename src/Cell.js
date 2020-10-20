925cab8e99734455710a6cb706d900a46cd55984
Xiaoyi Liu <lxy878@gmail.com>
2020-10-20 14:45:25 +0000

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class Cell extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            color: props.value
+        }
+    }
+
+    click = () => {
+        this.setState({
+            color:'#333'
+        })
+    }
+
+    render(){
+        return (<div className='cell' style={{backgroundColor: this.state.color}} onClick={this.click}></div>)
+    }
+}
